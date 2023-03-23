e20003cfdaa283f68b749e44ccfcee519473729b
pablozuta <pablo.zuta.h@gmail.com>
2023-03-23 01:06:53 -0400

    se agrega modulo 'sum two numbers'

---
+++ sum-two-numbers.scala
@@ -0,0 +1,9 @@
+object Sum {
+  def main(args: Array[String]): Unit = {
+    val numeroUno = 23
+    val numeroDos = 463
+    val resultado = numeroUno + numeroDos
+
+    println(s"La suma de $numeroUno y $numeroDos es: $resultado")
+  }
+}
