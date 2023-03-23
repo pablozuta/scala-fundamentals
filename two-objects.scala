48d842132f3a6e5f17631f3a5a068123cf3dbe45
pablozuta <pablo.zuta.h@gmail.com>
2023-03-23 01:20:17 -0400

    se agrega modulo 'two objects'

---
+++ two-objects.scala
@@ -0,0 +1,20 @@
+object Program {
+  def main(args: Array[String]): Unit = {
+   
+
+object YearOfGlad {
+  def frase(): Unit = {
+    println("I am seated in an office, surrounded by heads and bodies.")
+  }
+}
+
+object YearOfTheDependAdultUnderGarmet {
+  def frase(): Unit = {
+    println("Where was the woman who said she'd come. She said she would come. Erdedy thought she'd have come by now. He sat and thought. He was in the living room.")
+  }
+}
+
+YearOfGlad.frase()
+YearOfTheDependAdultUnderGarmet.frase()
+  }
+}
